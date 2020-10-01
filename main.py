5d1684002ea7ece8c1e408a5719b9f24565a257c
Gideon Akilas <48100779+gideonakilas@users.noreply.github.com>
2020-10-01 11:02:26 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,34 @@
-import Adafruit_DHT
 import time
+import board
+import adafruit_dht
 
-DHT_SENSOR = Adafruit_DHT.DHT11
-DHT_PIN = 23
+# Initial the dht device, with data pin connected to:
+dhtDevice = adafruit_dht.DHT11(board.D23)
+
+# you can pass DHT22 use_pulseio=False if you wouldn't like to use pulseio.
+# This may be necessary on a Linux single board computer like the Raspberry Pi,
+# but it will not work in CircuitPython.
+# dhtDevice = adafruit_dht.DHT22(board.D18, use_pulseio=False)
 
 while True:
-    humidity, temperature = Adafruit_DHT.read(DHT_SENSOR, DHT_PIN)
-    if humidity is not None and temperature is not None:
-        print("Temp={0:0.1f}C Humidity={1:0.1f}%".format(temperature, humidity))
-    else:
-        print("Sensor failure. Check wiring");
-    time.sleep(3)
+    try:
+        # Print the values to the serial port
+        temperature_c = dhtDevice.temperature
+        temperature_f = temperature_c * (9 / 5) + 32
+        humidity = dhtDevice.humidity
+        print(
+            "Temp: {:.1f} F / {:.1f} C    Humidity: {}% ".format(
+                temperature_f, temperature_c, humidity
+            )
+        )
+
+    except RuntimeError as error:
+        # Errors happen fairly often, DHT's are hard to read, just keep going
+        print(error.args[0])
+        time.sleep(2.0)
+        continue
+    except Exception as error:
+        dhtDevice.exit()
+        raise error
+
+    time.sleep(1.0)
